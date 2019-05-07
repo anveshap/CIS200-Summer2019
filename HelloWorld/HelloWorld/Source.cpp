472b364bc7dbad7e1dc2cf971f838baf370e2d1c
Eric Charnesky <eric.charnesky@careevolution.com>
2019-05-07 10:19:07 -0400

    Talk to the person by name

--- HelloWorld/HelloWorld/Source.cpp
+++ HelloWorld/HelloWorld/Source.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
+	string name;
+	cout << "Enter your name Human: ";
+
+	cin >> name;
+	cout << "Hello " << name << " - I am the computer!" << endl;
 
 	system("pause");
 	return 0;
